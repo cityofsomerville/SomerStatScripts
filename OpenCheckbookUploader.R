ec2fe575acf9b3002d865ec00843ce881a240467
Michael Mastrobuoni <michaelmastrobuoni@gmail.com>
2016-07-15 11:19:15 -0400

    Add Open Checkbook Uploader
    baby's first script

---
+++ OpenCheckbookUploader.R
@@ -0,0 +1,22 @@
+## The intent of this script is to upload open checkbook data from the SomerStat Data drive and post it to the Socrata open data portal.
+## It is expected that this will be set up through Windows Task Scheduler to run Monthly, the open checkbook data is posted to the SomerStat Data drive monthly on the 5th of the month.
+
+## Set working Directory ##
+setwd("C:/Users/mmastrobuoni.CH2SOM-MMASTROB/Documents/GitHub/Somerville_Data_Pipes")
+
+# This pulls in the credentials you need
+source("./config.R")
+
+## Load Packages
+library(httr) #for PUT function
+
+## Load open checkbook data from the SomerStat Data drive and write locally (if necessary) 
+#checkbook <- read.csv("//fileshare1/Departments2/Somerstat Data/IT/csv_opencheckbook_2016.csv")
+#write.csv(checkbook, file = "checkbook.csv")
+
+## upload to Socrata
+PUT("https://data.somervillema.gov/resource/cyh9-gqxg.json",
+    body = upload_file("./checkbook.csv"),
+    authenticate(Socrata_username, Socrata_password), 
+    add_headers("X-App-Token" = Socrata_token,
+                "Content-Type" = "text/csv"))
